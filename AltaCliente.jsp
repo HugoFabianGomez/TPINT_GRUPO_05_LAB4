2bdd35ee55706173737244e2e3e2fd6a671a6d98
HugoFabianGomez <64662305+HugoFabianGomez@users.noreply.github.com>
2022-11-15 20:44:33 -0300

    Add files via upload
    Con Carga de usuario

---
+++ AltaCliente.jsp
@@ -0,0 +1,147 @@
+<%@page import="java.util.ArrayList"%>
+<%@page import="entidades.Nacionalidad"%>
+<%@page import="entidades.Provincia"%>
+<%@page import="entidades.Genero"%>
+<%@page import="entidades.Localidad"%>
+<%@page import="entidades.Cliente"%>
+<%@page import="presentacion.controller.ServletNacionalidades"%>
+<%@page import="presentacion.controller.ServletProvincias"%>
+<%@page import="presentacion.controller.ServletGeneros"%>
+<%@page import="presentacion.controller.ServletLocalidades"%>
+<%@page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Alta de Cliente</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<%
+
+ArrayList<Genero> list_Genero = (ArrayList<Genero>) ServletGeneros.obtenerGeneros();
+ArrayList<Nacionalidad> list_Nacionalidad = (ArrayList<Nacionalidad>) ServletNacionalidades.obtenerNacionalidades();
+ArrayList<Provincia> list_Provincia = (ArrayList<Provincia>) ServletProvincias.obtenerProvincia();
+ArrayList<Localidad> list_Localidad = (ArrayList<Localidad>) ServletLocalidades.obtenerLocalidades();
+
+%>
+
+	<jsp:include page="Menu.html"></jsp:include>
+	<h3>Alta de Cliente</h3>
+<form action="ServletClientes" method="post">
+<table>
+	<tr> 
+		<td style="width: 139px; ">DNI</td>
+		<td style="height: 0px; "><input type="Number" name="txtDNI" min="1" max="99999999"
+		required  maxlength="10" autocomplete="off" onkeypress="return((event.charCode >= 48 && event.charCode <= 57))"
+		value="00.000.000" /><br></td>
+		<td>CUIL  </td>
+		<td style="height: 0px; "><input type="Number" name="txtCUIL" 
+		required  maxlength="10" autocomplete="off" onkeypress="return((event.charCode >= 48 && event.charCode <= 57))"
+		value="00-00000000-0" /><br></td>
+
+	</tr>
+		<tr> 
+		<td>Nombre(s): </td>
+		<td style="height: 0px; "><input type="text" name="txtNOMBRE" style="text-transform:uppercase"/></td>
+		<td>Apellido(s): </td>
+		<td style="height: 0px; "><input type="text" name="txtAPELLIDO" style="text-transform:uppercase"/></td>
+	</tr>
+	 
+	<tr> 
+		<td>Nacionalidad: </td>
+		<td style="height: 0px; ">
+			<select name="txtNACIONALIDAD" required>
+			
+				<%
+				if(list_Nacionalidad!=null)
+					for (Nacionalidad nac : list_Nacionalidad) {
+				%>
+					<option value="<%=nac.getCodigo()%>"><%=nac.getDescripcion()%></option>
+				<%
+					}
+					%>
+								 				
+			 </select>	
+		</td>
+		<td>Fecha de Nacimiento: </td>
+		<td style="height: 0px; "><input type="date" name="txtFECHA_NAC" value="DD/MM/AAAA" required/><br></td>
+		<td>Género: </td>
+		<td style="height: 0px; ">
+			<select name="txtGENERO" required >
+				<%
+				if(list_Genero!=null)
+					for (Genero gen : list_Genero) {
+				%>
+					<option value="<%=gen.getCodigo()%>"><%=gen.getDescripcion()%></option>
+				<%
+					}
+					%>
+
+			 </select>	
+		</td>
+		
+	</tr>
+	
+	<tr>
+		<td>Domicilio: </td>
+		<td style="height: 0px; "><input type="text" name="txtDIRECCION" required  maxlength="40" autocomplete="off" style="text-transform:uppercase"/><br></td>
+		<td style="height: 0px; ">
+			<select name="txtLOCALIDAD" >
+			<option value="1" >Localidad: </option>
+			
+				<%
+				if(list_Localidad!=null)
+					for (Localidad loc : list_Localidad) {
+				%>
+					<option value="<%=loc.getCodigo()%>"><%=loc.getDescripcion()%></option>
+				<%
+					}
+					%>
+					<!-- ACA DESARROLLO LAS OPCIONES -->									 				
+			 </select>	
+		</td>
+		<td style="height: 0px; ">
+			<select name="txtPROVINCIA" >
+			
+			<%
+				if(list_Provincia!=null)
+					for (Provincia prov : list_Provincia) {
+				%>
+					<option value="<%=prov.getCodigo()%>"><%=prov.getDescripcion()%></option>
+				<% } %>
+			
+					<!-- ACA DESARROLLO LAS OPCIONES -->									 				
+			 </select>	
+		</td>
+	</tr>
+	<tr>
+		<td>Correo Electrónico: </td>
+		<td style="height: 0px; ">  <input type="email" name="textEMAIL" required /> </td>	
+	</tr>
+	<tr> 
+		<td>Teléfono: </td>
+		<td style="height: 0px; "><input type="tel" name="txtTELEFONO"/></td>
+	</tr>
+	<tr>
+		<td>Usuario: </td>
+		<td style="height: 0px; ">  <input type="text" name="textUSUARIO" required /> </td>	
+		<td> <input type="text" readonly value="CLAVE = DNI" /></td>
+	</tr>
+	
+	
+	<tr>
+		<td></td>
+	</tr>
+	<br />
+	<tr>
+		<td style="height: 0px; "><input type="submit" name="btnAceptar" value="Agregar"/></td>
+	</tr>
+
+</table>
+</form>
+
+</body>
+</html>
---
+++ ServletClientes.java
@@ -0,0 +1,180 @@
+package presentacion.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entidades.Cliente;
+import entidades.Nacionalidad;
+import entidades.Provincia;
+import entidades.Usuario;
+import negocio.ClienteNegocio;
+import negocio.UsuarioNegocio;
+import negocioImpl.ClienteNegocioImpl;
+import negocioImpl.UsuarioNegocioImpl;
+import entidades.Genero;
+import entidades.Localidad;
+
+import daoImpl.ClienteDaoImpl;
+import entidades.Cliente;
+//>>>>>>> 5b4a8aa43b3a66077f920fb03041551279e015b3
+
+@WebServlet("/ServletClientes")
+public class ServletClientes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	static ClienteNegocio ClienteNeg = new ClienteNegocioImpl();
+	static UsuarioNegocio UsuaNeg = new UsuarioNegocioImpl();
+
+	public ServletClientes() {
+		super();
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(request.getParameter("btnAceptar")!=null)
+		{
+			boolean filas = false;
+			System.out.println("Entro Servlet");
+			
+			Usuario usu = new Usuario();
+			usu.setNombreUsuario(request.getParameter("textUSUARIO"));
+			usu.setContrasenia(request.getParameter("txtDNI"));
+			filas= UsuaNeg.agregar(usu);
+			
+			
+			
+			Cliente cliente =  new Cliente();
+			cliente.setDni(Integer.parseInt(request.getParameter("txtDNI")));
+			cliente.setCuil(Integer.parseInt(request.getParameter("txtCUIL")));
+			cliente.setNombre(request.getParameter("txtNOMBRE"));
+			cliente.setApellido(request.getParameter("txtAPELLIDO"));
+			cliente.setNacionalidad(new Nacionalidad(Integer.parseInt(request.getParameter("txtNACIONALIDAD"))));
+			cliente.setFechaNacimiento(request.getParameter("txtFECHA_NAC"));
+			cliente.setGenero(new Genero(Integer.parseInt(request.getParameter("txtGENERO"))));
+			cliente.setDomicilio(request.getParameter("txtDIRECCION"));
+			cliente.setLocalidad(new Localidad(Integer.parseInt(request.getParameter("txtLOCALIDAD"))));
+			cliente.setProvincia(new Provincia(Integer.parseInt(request.getParameter("txtPROVINCIA"))));
+			cliente.setEmail(request.getParameter("textEMAIL"));
+			cliente.setTelefono(request.getParameter("txtTELEFONO"));
+			cliente.setUsuario(new Usuario(request.getParameter("textUSUARIO")));
+			
+			
+			//System.out.println(cliente.getFechaNacimiento());
+			
+			filas=ClienteNeg.agregar(cliente);
+			//REQUESTDISPATCHER
+			
+			request.setAttribute("filas", filas);
+			RequestDispatcher rd = request.getRequestDispatcher("/ListarClientes.jsp");   
+	        rd.forward(request, response);
+	        
+		} else if(request.getParameter("btnModificar")!=null) 
+		{
+			Cliente cl=  new Cliente();
+			int dni = Integer.parseInt(request.getParameter("dni"));
+			
+			cl.setDni(dni);
+			System.out.println("dni= "+dni);
+			cl.setCuil(Integer.parseInt(request.getParameter("txtCUIL")));
+			
+			cl.setNombre(request.getParameter("txtNOMBRE"));
+			cl.setApellido(request.getParameter("txtAPELLIDO"));
+			cl.setNacionalidad(new Nacionalidad(Integer.parseInt(request.getParameter("txtNACIONALIDAD"))));
+			cl.setFechaNacimiento(request.getParameter("txtFECHA_NAC"));
+			cl.setGenero(new Genero(Integer.parseInt(request.getParameter("txtSEXO"))));
+			cl.setDomicilio(request.getParameter("txtDIRECCION"));
+			cl.setLocalidad(new Localidad(Integer.parseInt(request.getParameter("txtLOCALIDAD"))));
+			cl.setProvincia(new Provincia(Integer.parseInt(request.getParameter("txtPROVINCIA"))));
+			cl.setEmail(request.getParameter("textEMAIL"));
+			cl.setTelefono(request.getParameter("txtTELEFONO"));
+
+			boolean filas = ClienteNeg.modificar(cl);
+			
+			request.setAttribute("filas", filas);
+			request.setAttribute("dni", dni);
+			RequestDispatcher rd = request.getRequestDispatcher("/ModificarCliente.jsp");   
+	        rd.forward(request, response);  
+			
+		} else if(request.getParameter("btnEliminar")!=null) {
+			boolean Dni = false;
+			int dni =Integer.parseInt(request.getParameter("Dni"));
+			System.out.println("Enmtre a eliminar dni ="+dni);
+			Dni =ClienteNeg.borrar(Integer.parseInt(request.getParameter("Dni")));
+			
+			
+			request.setAttribute("eliminado", true);
+			request.setAttribute("filasEliminado", Dni);
+			RequestDispatcher rd = request.getRequestDispatcher("/ListarClientes.jsp");   
+	        rd.forward(request, response);
+			
+			
+		}
+		
+		
+	}
+	public static ArrayList<Cliente> obtenerClientes() {
+		return ClienteNeg.obtenerTodos();
+	}
+	
+	public static ArrayList<Cliente> obtenerClientes(String dni) {
+		System.out.println("Servlet");
+		return ClienteNeg.obtenerTodos(Integer.parseInt(dni));
+	}
+	
+	public static Cliente obtenerCliente(String dni) {
+		return ClienteNeg.obtenerUno(Integer.parseInt(dni));
+	}
+
+		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if(request.getParameter("btnModificar")!=null) 
+		{
+			Cliente cl=  new Cliente();
+			int dni = Integer.parseInt(request.getParameter("txtDNI"));
+			
+			cl.setDni(dni);
+			System.out.println("dni= "+dni);
+			cl.setCuil(Integer.parseInt(request.getParameter("txtCUIL")));
+			System.out.println("cuil= "+cl.getCuil());
+			cl.setNombre(request.getParameter("txtNOMBRE"));
+			System.out.println("nombre= "+cl.getNombre());
+			cl.setApellido(request.getParameter("txtAPELLIDO"));
+			System.out.println("apellido= "+cl.getApellido());
+			cl.setNacionalidad(new Nacionalidad(Integer.parseInt(request.getParameter("txtNACIONALIDAD"))));
+			System.out.println("nacionalidad= "+cl.getNacionalidad().getCodigo());
+			cl.setFechaNacimiento(request.getParameter("txtFECHA_NAC"));
+			System.out.println("fecha= "+cl.getFechaNacimiento());
+			cl.setGenero(new Genero(Integer.parseInt(request.getParameter("txtSEXO"))));
+			System.out.println("genero= "+cl.getGenero().getCodigo());
+			cl.setDomicilio(request.getParameter("txtDIRECCION"));
+			System.out.println("direccion= "+cl.getDomicilio());
+			cl.setLocalidad(new Localidad(Integer.parseInt(request.getParameter("txtLOCALIDAD"))));
+			System.out.println("localidad= "+cl.getLocalidad().getCodigo());
+			cl.setProvincia(new Provincia(Integer.parseInt(request.getParameter("txtPROVINCIA"))));
+			System.out.println("provincia= "+cl.getProvincia().getCodigo());
+			cl.setEmail(request.getParameter("textEMAIL"));
+			System.out.println("email= "+cl.getEmail());
+			cl.setTelefono(request.getParameter("txtTELEFONO"));
+			System.out.println("telefono= "+cl.getTelefono());
+
+			boolean filas = ClienteNeg.modificar(cl);
+			
+			request.setAttribute("filas", filas);
+			request.setAttribute("dni", dni);
+			RequestDispatcher rd = request.getRequestDispatcher("/ListarClientes.jsp");
+	        rd.forward(request, response);  
+			
+		}
+	}
+
+
+}
