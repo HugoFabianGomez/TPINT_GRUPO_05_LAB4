1c2b2b09d1943ec2cf882686686dfa3af0ae7db7
HugoFabianGomez <64662305+HugoFabianGomez@users.noreply.github.com>
2022-11-15 20:50:56 -0300

    Add files via upload

--- src/negocioImpl/UsuarioNegocioImpl.java
+++ src/negocioImpl/UsuarioNegocioImpl.java
@@ -6,10 +6,16 @@ import entidades.Usuario;
 import negocio.UsuarioNegocio;
 
 public class UsuarioNegocioImpl implements UsuarioNegocio {
-	UsuarioDao usu = new UsuarioDaoImpl();
+	UsuarioDao usuDao = new UsuarioDaoImpl();
 	
 	public Usuario usuario() {
 		return null;
+	}
+
+	@Override
+	public boolean agregar(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return usuDao.insertar(usuario);
 	}
 
 }
